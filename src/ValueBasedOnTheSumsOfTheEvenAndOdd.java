6d2743f50813cb0c2ebf17cf30c3c95b6edd7169
Elias-Kibret <eliaskibret2013n2@gmail.com>
2023-03-23 21:18:01 +0300

    Question

--- src/ValueBasedOnTheSumsOfTheEvenAndOdd.java
+++ src/ValueBasedOnTheSumsOfTheEvenAndOdd.java
@@ -1,3 +1,22 @@
+/* Write a function that takes an array of integers as an argument and
+ * a value based on the sums of the even and odd numbers in the array
+ * Let X=the sum of the odd numbers in the array and let Y=the sum of the even 
+ * numbers
+ * The function should return X-Y
+ * The signature of the function is 
+ * int f(int[] a)
+ * 
+ * Examples
+ * input : {1}
+ * output :1
+ * 
+ * input:{1,2}
+ * output: -1
+ * 
+ * input:{1,2,3,4}
+ * output: -2
+ */
+
 public class ValueBasedOnTheSumsOfTheEvenAndOdd {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4 };
