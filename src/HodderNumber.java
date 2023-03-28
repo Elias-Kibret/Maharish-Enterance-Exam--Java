eb18956fb0a73c313bd60649aefe3e845133d34f
Elias-Kibret <eliaskibret2013n2@gmail.com>
2023-03-29 00:50:28 +0300

    hodder number

--- src/HodderNumber.java
+++ src/HodderNumber.java
@@ -1,3 +1,20 @@
+/*A hodder number is one that is prime and is equal to 2j
+
+-1 for some j. For example, 31
+
+is a hodder number because 31 is prime and is equal to 25
+
+-1 (in this case j = 5). The first 4
+
+hodder numbers are 3, 7, 31, 127
+
+Write a function with signature int isHodder(int n) that returns 1 if n is a hodder
+number, otherwise it returns 0.
+Recall that a prime number is a whole number greater than 1 that has only two whole
+number factors, itself and 1.
+
+*/
+
 public class HodderNumber {
     public static void main(String[] args) {
         System.out.println(isHodder(31));
