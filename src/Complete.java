02d3694e442d2c08427032bc6c4bfafacb092a3a
Elias-Kibret <eliaskibret2013n2@gmail.com>
2023-03-30 11:19:28 +0300

    is completed

---
+++ src/Complete.java
@@ -0,0 +1,39 @@
+public class Complete {
+    public static void main(String[] args) {
+
+    }
+
+    public static int isComplete(int[] arr) {
+        if (arr.length < 2) {
+            return 0;
+        }
+        boolean isEven = false;
+        boolean isSquere = false;
+        boolean isSumTo8 = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                isEven = true;
+            }
+            int squareRoot = 0;
+            while (squareRoot <= arr[i]) {
+                if (squareRoot * squareRoot == arr[i]) {
+                    isSquere = true;
+                }
+                squareRoot++;
+            }
+            if (i <= arr.length - 1) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[j] + arr[i] == 8) {
+                        isSumTo8 = true;
+                    }
+                }
+            }
+        }
+        if (isEven && isSquere && isSumTo8) {
+            return 1;
+        } else {
+            return 0;
+        }
+
+    }
+}
